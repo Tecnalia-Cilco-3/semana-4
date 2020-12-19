5e092ed0223524606a00fb9d6570c6628c28c6e9
= <=>
2020-12-18 20:36:59 -0500

    Cambio eliminacion de s

--- migrations/20201218052411-create-articulo.js
+++ migrations/20201218052411-create-articulo.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Articulos', {
+    await queryInterface.createTable('Articulo', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -39,6 +39,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Articulos');
+    await queryInterface.dropTable('Articulo');
   }
 };
